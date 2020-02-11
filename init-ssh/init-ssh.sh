545ee353e5210088cc29ac58e804b4cabd35aa14
Joram van den Boezem <joram@vandenboezem.nl>
2020-02-11 15:23:27 +0100

    chore: add debug messages to script

--- init-ssh/init-ssh.sh
+++ init-ssh/init-ssh.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Create ~/.ssh folder"
 mkdir -p ~/.ssh
-echo -e "$PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod og-rwx ~/.ssh/id_rsa
-cp known_hosts ~/.ssh
 
-# debug
-ls -la ~/.ssh
+echo "Copy private key to ~/.ssh/id_rsa"
+echo -e "$PRIVATE_KEY" > ~/.ssh/id_rsa
+
+echo "Set proper permissions on ~/.ssh/id_rsa"
+chmod og-rwx ~/.ssh/id_rsa
+
+echo "Copy known_hosts to ~/.ssh"
+cp known_hosts ~/.ssh
