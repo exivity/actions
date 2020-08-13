e825f3ae51119a4d942c825032a90f89478efe3b
Joram van den Boezem <joram@vandenboezem.nl>
2020-08-14 00:40:16 +0200

    fix: chmod

--- db/init-db.sh
+++ db/init-db.sh
@@ -9,4 +9,5 @@ echo "Creating database"
 docker exec exivity-postgres createdb -U postgres $DB_NAME
 
 echo "Running migrations"
+chmod u+x bin/$MIGRATE_BIN
 ./bin/$MIGRATE_BIN -path=migrations -database="postgres://postgres:postgres@localhost:5432/${DB_NAME}?sslmode=disable" up
