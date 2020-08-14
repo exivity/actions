e365224c4ba0b1a999114354a6102374c9aa5291
Joram van den Boezem <joram@vandenboezem.nl>
2020-08-14 17:08:34 +0200

    chore: setx path

--- lib/start-postgres-windows.sh
+++ lib/start-postgres-windows.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Updating postgres config"
 echo max_prepared_transactions = 16 >> "C:\Program Files\PostgreSQL\12\data\postgresql.conf"	
-echo "::add-path::C:\Program Files\PostgreSQL\12\bin"	
+setx PATH "%path%;C:\Program Files\PostgreSQL\12\bin"
 
 echo "Starting postgres service"
 sc config postgresql-x64-12 start=demand
