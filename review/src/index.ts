1315c5436bfa52d7551c27a507c93f769a159e60
Joram van den Boezem <joram@vandenboezem.nl>
2021-02-19 10:37:54 +0100

    chore: revert me later

--- review/src/index.ts
+++ review/src/index.ts
@@ -8,6 +8,8 @@ async function run() {
     const { owner, component } = getRepository()
     const default_branch = getRef()
 
+    console.log('debug, remove me!!')
+
     // inputs
     const ghToken = getToken()
     const pull_request = parseInt(getInput('pull'), 10)
