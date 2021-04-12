629418e0547169daa4d87ffb173259ae443c7f09
Calli <bart1evers@gmail.com>
2021-04-12 17:04:37 +0200

    included dist

--- init-ssh/dist/index.js
+++ init-ssh/dist/index.js
@@ -265,7 +265,6 @@ exports.getBooleanInput = getBooleanInput;
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function setOutput(name, value) {
-    process.stdout.write(os.EOL);
     command_1.issueCommand('set-output', { name }, value);
 }
 exports.setOutput = setOutput;
