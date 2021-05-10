d5edf28f253364f4e67eb112dcaa3aa3734b5154
Calli <bart1evers@gmail.com>
2021-05-10 14:34:44 +0200

    remove adding tools.exivity.com to hosts and using insecure

--- dummy-data/dummy-data-binary.sh
+++ dummy-data/dummy-data-binary.sh
@@ -10,11 +10,9 @@ unameOut="$(uname -s)"
 case "${unameOut}" in
 	Linux*)     
         dd_bin=dummy-data
-        sudo -- sh -c "echo \"37.97.203.170 tools.exivity.com\" >> /etc/hosts"
 		;;
 	*)
         dd_bin=dummy-data.exe
-        echo "37.97.203.170 tools.exivity.com" >> C:\\Windows\\System32\\drivers\\etc\\hosts
 esac
 
 echo "Downloading $dd_bin"
@@ -22,7 +20,7 @@ if [[ -f $dd_bin ]]
 then
     echo "Already present, skip downloading"
 else
-    curl --insecure https://tools.exivity.com/$dd_bin -O
+    curl https://tools.exivity.com/$dd_bin -O
     chmod +x $dd_bin
 fi
 
