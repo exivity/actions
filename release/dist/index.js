c0ab9aedafa3a199f94dfa06796e72a6f799ea6c
Michiel <michiel_87@hotmail.com>
2023-04-04 18:32:24 +0200

    chore: remove mustache

--- release/dist/index.js
+++ release/dist/index.js
@@ -72332,9 +72332,9 @@ var import_core10 = __toESM(require_core());
 
 // release/src/common/formatChangelog.ts
 function formatPublicChangelog(version2, issues) {
-  debug(`issues: ${JSON.stringify(features(issues), null, 2)}}`);
-  debug(`features: ${JSON.stringify(features(issues), null, 2)}}`);
-  debug(`fixes: ${JSON.stringify(fixes(issues), null, 2)}}`);
+  debug(`issues: ${JSON.stringify(issues, null, 2)}`);
+  debug(`features: ${JSON.stringify(features(issues), null, 2)}`);
+  debug(`fixes: ${JSON.stringify(fixes(issues), null, 2)}`);
   return [
     `## ${version2}`,
     "",
