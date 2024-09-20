69739cfb8035344d10c7eb9309f9513ffcdd1f4b
Michiel <michiel_87@hotmail.com>
2024-09-20 12:29:58 +0200

    chore: add build iamge

---
+++ build-image/src/index.ts
@@ -0,0 +1,54 @@
+import { getInput, setFailed, getBooleanInput } from '@actions/core'
+import { table } from '../../lib/core'
+import {
+  dockerBuild,
+  dockerLogin,
+  dockerPush,
+  getImageFQN,
+} from '../../lib/dockerCli'
+import { getOwnerInput, getRepoInput } from '../../lib/github'
+import { branchToTag, getLabels } from '../../lib/image'
+import { writeMetadataFile } from './metadataFile'
+
+async function run() {
+  // Inputs
+  const namespace = getOwnerInput('namespace')
+  const name = getRepoInput('name')
+  const dockerfile = getInput('dockerfile')
+  const context = getInput('context')
+  const registry = getInput('registry')
+  const user = getInput('user')
+  const password = getInput('password')
+  const useSSH = getBooleanInput('useSSH')
+  const secrets = getInput('secrets')
+
+  // Get all relevant metadata for the image
+  const labels = getLabels(name)
+  const tag = branchToTag()
+  const image = { registry, namespace, name, tag }
+
+  table('Repository', getImageFQN(image))
+  table('Tag', tag)
+  table('Labels', JSON.stringify(labels, undefined, 2))
+
+  await writeMetadataFile(name)
+
+  await dockerLogin({
+    registry,
+    user,
+    password,
+  })
+
+  await dockerBuild({
+    dockerfile,
+    context,
+    labels,
+    image,
+    useSSH,
+    secrets,
+  })
+
+  await dockerPush(image)
+}
+
+run().catch(setFailed)
