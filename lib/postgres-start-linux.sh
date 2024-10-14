64391bd08d5c55c002597560a472910752315d7b
Steffen Exler <steffen@exivity.com>
2024-10-14 13:27:07 +0000

    chore(postgres): update config path for PostgreSQL 16
    Signed-off-by: Steffen Exler <steffen@exivity.com>

--- lib/postgres-start-linux.sh
+++ lib/postgres-start-linux.sh
@@ -3,14 +3,8 @@
 set -e
 
 echo "Updating postgres config"
-ls /etc/postgresql/
-ls /etc/postgresql/
-ls /etc/postgresql/
-ls /etc/postgresql/
-ls /etc/postgresql/
-ls /etc/postgresql/
-sudo -u postgres chmod 777 /etc/postgresql/14/main/postgresql.conf
-sudo echo max_prepared_transactions = 16 >> /etc/postgresql/14/main/postgresql.conf
+sudo -u postgres chmod 777 /etc/postgresql/16/main/postgresql.conf
+sudo echo max_prepared_transactions = 16 >> /etc/postgresql/16/main/postgresql.conf
 
 echo "Starting postgres service"
 sudo service postgresql start
