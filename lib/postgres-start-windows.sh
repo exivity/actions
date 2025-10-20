c78230c98674c8c5bdacd69db3a07b725fd6fc52
michiel <michiel_87@hotmail.com>
2025-10-20 15:36:23 +0200

    fix: update postgres path for windows-2022

--- lib/postgres-start-windows.sh
+++ lib/postgres-start-windows.sh
@@ -6,9 +6,10 @@ export LC_ALL=en_US.utf8
 echo "Get installed Postgres version"
 cd "C:/Program Files/PostgreSQL/"
 postgres_version=$(ls -d * | grep -oP '[0-9]*' | sort -nr | head -n 1)
+config_file="C:/PostgreSQL/${postgres_version}/data/postgresql.conf"
 
 echo "Updating Postgres ${postgres_version} config"
-echo "max_prepared_transactions = 16" >> "C:/Program Files/PostgreSQL/${postgres_version}/data/postgresql.conf"
+echo "max_prepared_transactions = 16" >> "${config_file}"
 
 echo "Starting Postgres ${postgres_version} service"
 sc config "postgresql-x64-${postgres_version}" start=demand
